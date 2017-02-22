4e89d175171ca17edcb3645fd16e6600c57339d8
Donald Tran <tran.donald@gmail.com>
2017-02-22 10:08:56 -0800

    Completed problem with all cases.

---
+++ practice29/application.js
@@ -0,0 +1,23 @@
+
+
+
+function do_math(num1, num2, operator){
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+    switch(operator){
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        case '/':
+            return num1 / num2;
+        case '*':
+        case 'x':
+        case 'X':
+            return num1 * num2;
+        default:
+            console.log("Please specify: (a number, a number, an operator).");
+            return "Thank you.";
+            break;
+    }
+}
