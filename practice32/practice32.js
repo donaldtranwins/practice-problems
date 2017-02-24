25cbb7bb5439f28c68dcf89a9f4434887ca1a5a4
Donald Tran <tran.donald@gmail.com>
2017-02-24 11:57:33 -0800

    Reorganized code

--- practice32/practice32.js
+++ practice32/practice32.js
@@ -3,22 +3,22 @@ function find_middle_letters(input){
 
     //String Methods
     if (string.length % 2 === 1) {
-        var oddString = string.charAt((string.length - 1) / 2);
-        return oddString;
+        var middleLetter = string.charAt((string.length - 1) / 2);
+        return middleLetter;
     } else {
-        var mid1 = string.charAt(string.length / 2 - 1);
-        var mid2 = string.charAt(string.length / 2);
-        var evenString = mid1 + mid2;
-        return evenString;
+        var middle1 = string.charAt(string.length / 2 - 1);
+        var middle2 = string.charAt(string.length / 2);
+        var middleLetters = middle1 + middle2;
+        return middleLetters;
 //             return string.charAt(string.length / 2 - 1) + string.charAt(string.length / 2);
     }
 
     // Array Methods
     var stringArray = string.split("");
     if (string.length % 2 === 0) {
-        var middle1 = stringArray[string.length / 2 - 1];
-        var middle2 = stringArray[string.length / 2];
-        var evenAnswer = middle1 + middle2;
+        var mid1 = stringArray[string.length / 2 - 1];
+        var mid2 = stringArray[string.length / 2];
+        var evenAnswer = mid1 + mid2;
         return evenAnswer;
         // return stringArray[string.length / 2 - 1] + stringArray[string.length / 2];
     } else {
