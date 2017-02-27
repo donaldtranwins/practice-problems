8240bb1bd2daa48d0e6e2e32c00e01b242a7e5c2
Donald Tran <tran.donald@gmail.com>
2017-02-27 10:51:14 -0800

    Completed problem, added jquery to HTML to handle answer

---
+++ snakeCaseConverter/snakeCaseConverter.js
@@ -0,0 +1,25 @@
+function snakeCaseConverter(string){
+    var arrayOfString = string.split(" ");
+    for (var i=0; i<arrayOfString.length; i++){
+        arrayOfString[i] = arrayOfString[i].toLowerCase();
+        var splitWord = arrayOfString[i].split("");
+        splitWord[0] = splitWord[0].toUpperCase();
+        var joinedWord = splitWord.join("");
+        arrayOfString[i] = joinedWord;
+    }
+    var joinedString = arrayOfString.join("_");
+    console.log(joinedString);
+    return joinedString;
+}
+function callFunction(){
+    var string = $('#input').val();
+    var answer = snakeCaseConverter(string);
+    $('#output').text(answer);
+}
+function allowEnterKey(){
+    if (event.keyCode === 13) callFunction();
+}
+$(document).ready(function(){
+   $('#call').click(callFunction);
+   $('#input').keypress(allowEnterKey);
+});
