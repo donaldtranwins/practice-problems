8c395cfb6d99f62dba243569d1df9c007eded8e8
Donald Tran <tran.donald@gmail.com>
2017-04-13 10:05:55 -0700

    small changes

--- banking/banking.js
+++ banking/banking.js
@@ -1,36 +1,35 @@
 function BankSystem (){
     this.banks = {};
+    this.deletedBanks = {};
     this.addBank = name => {
-        this.banks[name] = new Bank(name);
+        this.banks[name] = new Bank(name, this);
     };
     this.removeBank = name => {
-        let empty = {};
-        console.log(`The entire ${this.banks[name]['name']} bank has been removed from the system`);
-        this.banks[name] = empty;
+        this.deletedBanks[name] = this.banks[name];
         delete this.banks[name];
+        console.log(`The bank ${this.deletedBanks[name]['name']} has been removed from the system`);
     };
-    function Bank (name){
+    function Bank (name, system){
         this.name = name;
-        this.accounts = [null];
+        this.system = system;
+        this.accounts = {};
+        this.deletedAccounts = {};
         this.incrementer = 1;
-        this.addAccount = (name,initialDeposit) => {
-            let acc = new Account(name,initialDeposit);
-            this.accounts.push(acc);
-            let id = this.accounts.length-1;
-            this.accounts[id].id = id;
-            console.log(`New Account #${id} with ${acc.logFunds()} at ${this.name}`);
+        this.holdings = null;
+        this.addAccount = (name, initialDeposit) => {
+            let id = this.incrementer++;
+            this.accounts[id] = new Account(name, initialDeposit, this);
+            console.log(`New Account #${id} with ${this.accounts[id].logFunds()} at ${this.name}`);
         };
         this.removeAccount = id => {
-            let empty = {};
-            this.accounts[id] = empty;
-            let removed = this.accounts.splice(id,1);
-            console.log(`${removed.name}'s account has been removed from ${this.name}`);
+            this.deletedAccounts[id] = this.accounts[id]; //soft deleting
+            delete this.accounts[id];
+            console.log(`${this.deletedAccounts[id].name}'s account has been removed from ${this.name}`);
         };
 
-        function Account (name, initialDeposit){
+        function Account (name, initialDeposit = 0, bank){
             this.name = name;
-            if (!initialDeposit)
-                initialDeposit = 0;
+            this.bank = bank;
             this.funds = parseFloat(initialDeposit.toFixed(2)) || 0;
             this.addFunds = amount => {
                 if (amount < 0){
@@ -52,12 +51,45 @@ function BankSystem (){
                 console.log(this.logFunds(amount, 'removed'));
                 return amount; //@returns funds removed, will be less than what they specified
             };
-            this.transferBetweenAccounts = function(value, from, to){
-                let money = from.removeFunds(value);
-                to.addFunds(money);
+            this.transferBetweenAccounts = function(amount, id){
+                const targetAcc = this.bank.accounts[id];
+                const targetsOrigBalance = targetAcc.funds;
+                const money = this.removeFunds(amount);
+                this.bank.holdings = money;
+                targetAcc.addFunds(this.bank.holdings);
+                if (targetAcc.funds === targetsOrigBalance+this.bank.holdings){
+                    console.log("Transaction successful!");
+                } else {
+                    console.log("Mismatch in funds transferred. Refunding values");
+                    targetAcc.funds = targetsOrigBalance;
+                    this.addFunds(money);
+                    /**
+                     * This part of the function (to validate if the funds are actually transferred correctly)
+                     * is unnecessary in this example, because everything runs synchronously.  but in a real
+                     * world situation, this function  would make a server request and some sort of asynchronous
+                     * validation.  If the internet dies during that time or if the validation is unsuccessful,
+                     * we wouldnt want the user to lose their money, nor the target person to gain extra money
+                     *
+                     * Thus, this two step-check allows for reversion of funds back to their original values.
+                     */
+                }
+                this.bank.holdings = null;
             };
-            this.transferBetweenBanks = function(){
-
+            this.transferBetweenBanks = function(amount, id) {
+                //@TODO this is copy pasta of above.  modify it to do the thing
+                const targetAcc = this.bank.accounts[id];
+                const targetsOrigBalance = targetAcc.funds;
+                const money = this.removeFunds(amount);
+                this.bank.holdings = money;
+                targetAcc.addFunds(this.bank.holdings);
+                if (targetAcc.funds === targetsOrigBalance + this.bank.holdings) {
+                    console.log("Transaction successful!");
+                } else {
+                    console.log("Mismatch in funds transferred. Refunding values");
+                    targetAcc.funds = targetsOrigBalance;
+                    this.addFunds(money);
+                }
+                this.bank.holdings = null;
             };
             this.logFunds = (amount, transaction) => {
                 if (amount && transaction){
